0d17463f293d92c93abaf984b4d6be00e0f5eb7e
Pär Karlsson <par.karlsson@cybercom.com>
2016-02-12 10:35:20 +0100

    restructured files

--- gountries/query.go
+++
@@ -1,9 +0,0 @@
-package gountries
-
-// Query holds a reference to the QueryHolder struct
-var query *QueryHolder
-
-// QueryHolder contains queries for countries, cities, etc.
-type QueryHolder struct {
-	Countries []Country
-}
